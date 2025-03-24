f34ab546fd90bda216f7ef08c110247c39dff92d
Dima Ivaniuta <divanyuta@adigida.com>
2025-03-24 14:47:24 +0100

    Angular up module

--- carsell-frontend/src/app/app.module.ts
+++ carsell-frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';git commit -m "Your commit message describing the changes"
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
