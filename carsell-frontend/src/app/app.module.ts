7b5fd54b790fae27599ffc736b7388a8da4abcd4
Dima Ivaniuta <divanyuta@adigida.com>
2025-03-24 14:46:11 +0100

    Carsell - Platform initial project commit

---
+++ carsell-frontend/src/app/app.module.ts
@@ -0,0 +1,56 @@
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// Angular Material modules
+import { MatToolbarModule } from '@angular/material/toolbnpm installar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatDialogModule } from '@angular/material/dialog';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+// Components
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { CarListComponent } from './components/cars/car-list.component';
+import { CarDialogComponent } from './components/cars/car-dialog.component';
+
+// Services & Interceptors
+import { JwtInterceptor } from './shared/jwt-interceptor';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    UserProfileComponent,
+    CarListComponent,
+    CarDialogComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    MatTableModule,
+    MatDialogModule
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
